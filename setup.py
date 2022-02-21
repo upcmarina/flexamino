b3f287add49dcdd6cded9a99f5019e286d8f71f7
Marina <67465839+upcmarina@users.noreply.github.com>
2022-02-21 17:55:00 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+
+### Created on 
+### Authors
+
+setup(name=’ProjectName',
+  version='1.0',
+  description=’Flexibility Scorer',
+  author=’Eric Toro, Marina Vallejo, Sara Vega',
+  author_email=’@estudiant.upf.edu, marina.vallejo01@estudiant.upf.edu, @estudiant.upf.edu',
+  url=’http://www.url_of_my_project.org',
+  packages=[’my_package', ’my_package.subpackage'],
+)
