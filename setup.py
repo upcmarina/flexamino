8c4f2f65e6891e87350b6aaaef6ca6ee1b28324a
etd530 <85890746+etd530@users.noreply.github.com>
2022-02-21 18:01:36 +0100

    added our emails

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name=’ProjectName',
   version='1.0',
   description=’Flexibility Scorer',
   author=’Eric Toro, Marina Vallejo, Sara Vega',
-  author_email=’@estudiant.upf.edu, marina.vallejo01@estudiant.upf.edu, @estudiant.upf.edu',
+  author_email=’eric.toro01@estudiant.upf.edu, marina.vallejo01@estudiant.upf.edu, sara.vega02@estudiant.upf.edu',
   url=’http://www.url_of_my_project.org',
   packages=[’my_package', ’my_package.subpackage'],
 )
