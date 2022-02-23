2c52ba524073143ccd2b15267036ac20f3e9454f
Marina <67465839+upcmarina@users.noreply.github.com>
2022-02-23 18:47:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
-
-
-### Created on 21/02/2022
-### Authors : Toro; Vallejo; Vega
+"""
+setup.py
+Created on 21/02/2022
+Authors : Toro; Vallejo; Vega
+"""
 
 setup(name='FlexibleProteins',
   version='1.0',
   description='Tool to obtain Flexibility Scores for each aminoacid in a given sequence',
   long_description=open('README.md').read(),
-  author='Eric Toro, Marina Vallejo, Sara Vega',
+  author='Eric Toro Delgado, Marina Vallejo Vallés, Sara Vega Abellaneda',
   author_email='eric.toro01@estudiant.upf.edu, marina.vallejo01@estudiant.upf.edu, sara.vega02@estudiant.upf.edu',
   url='http://www.url_of_my_project.org',
   install_requires = [ ]
-  packages=[ ],
-  keywords = 'bioinformatics protein_flexibility b_factors'
+  packages=['ProgramName'],
+  keywords = 'bioinformatics protein_flexibility b_factors '
 )
