1944d49100de0453c3cabaeb56b58c6f47a23dd0
Marina <rinetix.upc@gmail.com>
2022-03-16 12:44:51 +0000

    Deepnote commit test

---
+++ advanced_models.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+""" 
+Package with functions to deal with Alpha fold. 
+by: Toro, Vallejo, Vega
+2022
+"""
+
+from input_reader import * 
+from pdb_functions import * 
+from profiles import * 
+from report import * 
+from urllib.request import urlopen
+
+import os
+import shutil
+
+
+
+def run_alpha_fold(query_uniprot):
+    """
+    Generate an alpha fold model of the query sequence.
+    """
+    url = "https://alphafold.ebi.ac.uk/files/AF-"+query_uniprot+"-F1-model_v2.pdb"
+    filehandle = urlopen(url)
+    pdbfile = open("./tmp/"+ query_uniprot +"_alphaFold.pdb", 'wb')
+    
+    for line in filehandle:
+        pdbfile.write(line)
+    pdbfile.close()
+
+    return "./tmp/"+ query_uniprot +"_alphaFold.pdb"
